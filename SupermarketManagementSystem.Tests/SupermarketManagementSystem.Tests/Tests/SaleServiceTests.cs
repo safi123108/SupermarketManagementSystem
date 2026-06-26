719e907bbb9a1713fdf7c92991d4a4610ce0c429
safi123108 <syedsafiullahinayat@gmail.com>
2026-06-27 01:01:11 +0500

    Add files via upload

---
+++ SupermarketManagementSystem.Tests/SupermarketManagementSystem.Tests/Tests/SaleServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace SupermarketManagementSystem.Tests
+{
+    public class SaleServiceTests
+    {
+        [Fact]
+        public void Quantity_Sold_Should_Be_Greater_Than_Zero()
+        {
+            int quantity = 5;
+
+            Assert.True(quantity > 0);
+        }
+    }
+}
