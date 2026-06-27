9d38e711f8f18f08358104557fcc8528c9654df2
safi123108 <syedsafiullahinayat@gmail.com>
2026-06-27 21:24:55 +0500

    Add files via upload

---
+++ SupermarketManagementSystem.Tests/Tests/ProductServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace SupermarketManagementSystem.Tests
+{
+    public class ProductServiceTests
+    {
+        [Fact]
+        public void Product_Can_Be_Created()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Product_Price_Should_Be_Positive()
+        {
+            decimal price = 100;
+
+            Assert.True(price > 0);
+        }
+    }
+}
---
+++ SupermarketManagementSystem.Tests/Tests/SupplierServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace SupermarketManagementSystem.Tests
+{
+    public class SupplierServiceTests
+    {
+        [Fact]
+        public void Supplier_Name_Should_Not_Be_Empty()
+        {
+            string name = "ABC Suppliers";
+
+            Assert.False(string.IsNullOrWhiteSpace(name));
+        }
+    }
+}
